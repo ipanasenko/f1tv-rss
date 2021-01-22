cce855b3882b2c5077d9e871720be6bfbacecafd
Ilya Panasenko <hottdogg2008@gmail.com>
2021-01-23 01:39:01 +0200

    first version of feed

--- api/rss.ts
+++ api/rss.ts
@@ -1,14 +1,45 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import RSS from 'rss';
+import { Feed } from 'feed';
+import { launch } from 'puppeteer';
 
-export default (req: NowRequest, res: NowResponse) => {
-  const rss = new RSS({
-    title: 'F1 TV',
-    feed_url: '',
-    site_url: 'https://f1tv.formula1.com/en/home',
+interface Episode {
+  image_urls: { type: string; url: string }[];
+  uid: string;
+  created: string;
+  title: string;
+  slug: string;
+  synopsis: string;
+}
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const feed = new Feed({
+    title: 'F1 TV RSS Feed',
+    id: 'https://f1tv.formula1.com/en/home',
+    copyright: '',
+    favicon: 'https://f1tv.formula1.com/assets/favicons/favicon.ico?v=1-30-0',
   });
 
-  rss.item({ title: 'dummy item', description: 'd i', url: 'http://example.com', date: new Date().toLocaleString() });
+  const browser = await launch({ headless: false });
+  const page = await browser.newPage();
 
-  res.status(200).send(rss.xml());
+  page.on('requestfinished', async (request) => {
+    if (request.method() === 'GET' && request.url().includes('/episodes/epis_')) {
+      const episode: Episode = (await request.response().json()) as Episode;
+      console.log('episode', episode);
+
+      feed.addItem({
+        title: episode.title,
+        description: episode.synopsis,
+        link: `https://f1tv.formula1.com/en/episode/${episode.slug}`,
+        date: new Date(episode.created),
+        image: episode.image_urls[0].url,
+      });
+    }
+  });
+
+  await page.goto('https://f1tv.formula1.com/en/home', { waitUntil: 'networkidle2' });
+
+  await browser.close();
+
+  res.status(200).send(feed.atom1());
 };
