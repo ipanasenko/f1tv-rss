775e219de449d1a16e04ae8a0e6c09ee1c8c4af5
Ilya Panasenko <hottdogg2008@gmail.com>
2021-01-23 00:44:58 +0200

    return dummy rss

--- api/rss.ts
+++ api/rss.ts
@@ -1,7 +1,14 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
-}
+import { NowRequest, NowResponse } from '@vercel/node';
+import RSS from 'rss';
+
+export default (req: NowRequest, res: NowResponse) => {
+  const rss = new RSS({
+    title: 'F1 TV',
+    feed_url: '',
+    site_url: 'https://f1tv.formula1.com/en/home',
+  });
+
+  rss.item({ title: 'dummy item', description: 'd i', url: 'http://example.com', date: new Date().toLocaleString() });
+
+  res.status(200).send(rss.xml());
+};
